5f28ef81afa0ee14035470a957fca8a980e144d0
Nuno Gonçalves <nunojfg@gmail.com>
2020-03-29 13:18:36 +0100

    Update WrapViewRepresentable.swift

--- WrapViewRepresentable.swift
+++ WrapViewRepresentable.swift
@@ -24,28 +24,3 @@ extension WrapViewRepresentable {
         update(view, context)
     }
 }
-
-struct Wrap<Wrapped: UIView>: WrapViewRepresentable {
-    
-    var makeView: () -> Wrapped
-    var update: (Wrapped, UIViewRepresentableContext<Wrap<Wrapped>>) -> Void
-
-    init(_ makeView: @escaping @autoclosure () -> Wrapped,
-         updater update: @escaping Updater) {
-        self.makeView = makeView
-        self.update = update
-    }
-}
-
-extension Wrap {
-    init(_ makeView: @escaping @autoclosure () -> Wrapped,
-         updater update: @escaping (Wrapped) -> Void) {
-        self.makeView = makeView
-        self.update = { view, _ in update(view) }
-    }
-
-    init(_ makeView: @escaping @autoclosure () -> Wrapped) {
-        self.makeView = makeView
-        self.update = { _, _ in }
-    }
-}
